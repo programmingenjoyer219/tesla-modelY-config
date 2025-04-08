662f63703b449db99ecce3e2182448afd7463c90
Yuganshu <yuganshu219@gmail.com>
2025-04-08 18:47:31 +0530

    create ConfigOption

---
+++ src/features/config-menu/components/ConfigOption.svelte
@@ -0,0 +1,18 @@
+<script>
+	/**
+	 * @type {{optionType: 'checkbox' | 'radio'; description: string; id: string;}}
+	 */
+	var { optionType, description, id } = $props();
+</script>
+
+<div class="flex items-center gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md">
+	<input
+		{id}
+		class={[
+			'size-6 appearance-none border-2 border-slate-500 checked:bg-blue-300',
+			optionType == 'radio' ? 'rounded-full' : 'rounded-sm'
+		]}
+		type={optionType}
+	/>
+	<span>{description}</span>
+</div>
