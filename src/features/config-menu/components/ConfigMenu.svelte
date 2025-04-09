07c5b0503d04cdb55b04b7228f2eeedd4b7c9d27
Yuganshu <yuganshu219@gmail.com>
2025-04-09 17:41:59 +0530

    discard ConfigOptionMenu

--- src/features/config-menu/components/ConfigMenu.svelte
+++ src/features/config-menu/components/ConfigMenu.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { carColor } from '../states/carColor.svelte';
 	import ColorSelectionMenu from './ColorSelectionMenu.svelte';
-	import ConfigOptionMenu from './ConfigOptionMenu.svelte';
 </script>
 
 <section class="space-y-8">
@@ -33,55 +32,100 @@
 			bind:selectedColor={carColor.interior}
 		/>
 		<!-- wheels -->
-		<ConfigOptionMenu
-			title="Wheels"
-			optionType="radio"
-			configOptions={[
-				{ id: 'standard', description: 'Standard' },
-				{ id: 'performance', description: 'Performance' }
-			]}
-		/>
+		<div class="space-y-2">
+			<p class="font-semibold">Wheels</p>
+
+			<div
+				class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+			>
+				<label class="flex-1" for="standard">Standard</label>
+				<input
+					id="standard"
+					class="size-6 appearance-none rounded-full border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300"
+					type="radio"
+				/>
+			</div>
+
+			<div
+				class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+			>
+				<label class="flex-1" for="performance">Performance</label>
+				<input
+					id="performance"
+					class="size-6 appearance-none rounded-full border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300"
+					type="radio"
+				/>
+			</div>
+		</div>
 		<!-- full self-driving -->
-		<ConfigOptionMenu
-			title="Full Self-Driving"
-			optionType="checkbox"
-			configOptions={[
-				{
-					id: 'full-self-driving',
-					description: 'Full Self-Driving (+$8500)'
-				}
-			]}
-		/>
+		<div class="space-y-2">
+			<p class="font-semibold">Full Self-Driving</p>
+
+			<div
+				class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+			>
+				<label class="flex-1" for="full-self-driving">Full Self-Driving (+$8500)</label>
+				<input
+					id="full-self-driving"
+					class="size-6 appearance-none rounded-sm border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300"
+					type="checkbox"
+				/>
+			</div>
+		</div>
 		<!-- performance package -->
-		<ConfigOptionMenu
-			title="Performance Package"
-			optionType="checkbox"
-			configOptions={[
-				{
-					id: 'performance-package',
-					description: 'Performance Package (+$5000)'
-				}
-			]}
-		/>
+		<div class="space-y-2">
+			<p class="font-semibold">Performance Package</p>
+
+			<div
+				class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+			>
+				<label class="flex-1" for="performance-package">Performance Package (+$5000)</label>
+				<input
+					id="performance-package"
+					class="size-6 appearance-none rounded-sm border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300"
+					type="checkbox"
+				/>
+			</div>
+		</div>
 		<!-- accessories -->
-		<ConfigOptionMenu
-			title="Accessories"
-			optionType="checkbox"
-			configOptions={[
-				{
-					id: 'center-console-tray',
-					description: 'Center Console Tray (+$35)'
-				},
-				{
-					id: 'sunshade',
-					description: 'Sunshade (+$105)'
-				},
-				{
-					id: 'all-weather-interior-liners',
-					description: 'All Weather Interior Liners (+$225)'
-				}
-			]}
-		/>
+		<div class="space-y-2">
+			<p class="font-semibold">Accessories</p>
+
+			<div
+				class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+			>
+				<label class="flex-1" for="center-console-tray">Center Console Tray (+$35)</label>
+				<input
+					id="center-console-tray"
+					class="size-6 appearance-none rounded-sm border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300"
+					type="checkbox"
+				/>
+			</div>
+
+			<div
+				class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+			>
+				<label class="flex-1" for="sunshade">Sunshade (+$105)</label>
+				<input
+					id="sunshade"
+					class="size-6 appearance-none rounded-sm border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300"
+					type="checkbox"
+				/>
+			</div>
+
+			<div
+				class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+			>
+				<label class="flex-1" for="all-weather-interior-liners"
+					>All Weather Interior Liners (+$225)</label
+				>
+				<input
+					id="all-weather-interior-liners"
+					class="size-6 appearance-none rounded-sm border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300"
+					type="checkbox"
+				/>
+			</div>
+		</div>
 	</section>
 	<div class="h-1 w-full rounded bg-slate-300/75"></div>
 	<!-- total price -->
