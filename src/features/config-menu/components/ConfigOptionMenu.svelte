c21695c0864dd7608c632fb50e3c5e1e27b295d4
Yuganshu <yuganshu219@gmail.com>
2025-04-09 13:30:53 +0530

    replace 'ConfigOption' with snippet

--- src/features/config-menu/components/ConfigOptionMenu.svelte
+++ src/features/config-menu/components/ConfigOptionMenu.svelte
@@ -1,6 +1,4 @@
 <script>
-	import ConfigOption from './ConfigOption.svelte';
-
 	/**
 	 * @typedef {Object} ConfigOption
 	 * @property {string} id
@@ -14,9 +12,29 @@
 	var { title, configOptions } = $props();
 </script>
 
+{#snippet configOption(
+	/** @type {string} */ id,
+	/** @type {string} */ description,
+	/** @type {'radio' | 'checkbox'} */ optionType
+)}
+	<div
+		class="flex flex-row-reverse items-center justify-end gap-4 rounded-md border-2 border-slate-400/75 p-3 shadow-md"
+	>
+		<label class="flex-1" for={id}>{description}</label>
+		<input
+			{id}
+			class={[
+				'size-6 appearance-none border-2 border-slate-500 outline-offset-4 checked:bg-blue-300 focus:outline-blue-300',
+				optionType == 'radio' ? 'rounded-full' : 'rounded-sm'
+			]}
+			type={optionType}
+		/>
+	</div>
+{/snippet}
+
 <div class="space-y-2">
 	<p class="font-semibold">{title}</p>
 	{#each configOptions as { id, description, optionType } (id)}
-		<ConfigOption {id} {description} {optionType} />
+		{@render configOption(id, description, optionType)}
 	{/each}
 </div>
