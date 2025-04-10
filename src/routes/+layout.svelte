1f5cfc18568022c131f8959cea195daf0e39ba9b
Yuganshu <yuganshu219@gmail.com>
2025-04-10 12:22:43 +0530

    pre-load LCP images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,8 @@
 <svelte:head>
 	<title>Tesla Model Y</title>
 	<meta name="description" content="Tesla Model Y configuration interface built using SvelteKit" />
+	<link rel="preload" as="image" href="/assets/model-y/exterior/ultra-red.avif" />
+	<link rel="preload" as="image" href="/assets/model-y/interior/dark.avif" />
 </svelte:head>
 
 <main class="inline-wrapper flow">
